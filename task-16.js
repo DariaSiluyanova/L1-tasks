e0df43b0f2bbd42c7c802db48771af0bb4c0f0da
Daria <prog-1@goodfil.com>
2023-09-07 15:44:31 +0300

    17todo

---
+++ task-16.js
@@ -0,0 +1,18 @@
+// 16. Задача на модули и использование внешних библиотек: напишите модуль,
+// который экспортирует функцию для работы с датами. Внутри модуля
+// используйте внешнюю библиотеку Moment.js для удобной работы с датами.
+import moment from 'moment';
+
+export function formatDate(date, location) {
+    function locale(){
+        if(location === "Россия") {
+            moment.locale("ru")
+        }
+        if(location === "Англия") {
+            moment.locale("en-au")
+        }
+    }
+    locale()
+
+    return moment(date).format("DD MMMM YYYY HH:mm");
+}
---
+++ test-16.js
@@ -0,0 +1,11 @@
+import { formatDate } from "./task-16.js";
+
+const date = {
+    years: 2010,
+    months: 10,
+    date: 12,
+    hours: 20,
+    minutes: 30,
+}
+
+console.log(formatDate(date, "Россия"))
